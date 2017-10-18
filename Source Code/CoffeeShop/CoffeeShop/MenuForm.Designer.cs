11951a58939e63b2f3916f964b283c47a1cf5da7
nvhungkt <ah.hung.ah@gmail.com>
2017-10-18 16:05:05 +0700

    Update GUI MenuForm.Designer

--- Source Code/CoffeeShop/CoffeeShop/MenuForm.Designer.cs
+++ Source Code/CoffeeShop/CoffeeShop/MenuForm.Designer.cs
@@ -28,21 +28,221 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dgvMenu = new System.Windows.Forms.DataGridView();
+            this.dgvCategory = new System.Windows.Forms.DataGridView();
+            this.groupContent = new System.Windows.Forms.GroupBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDeactive = new System.Windows.Forms.Button();
+            this.btnAddNew = new System.Windows.Forms.Button();
+            this.lbModifiedTime = new System.Windows.Forms.Label();
+            this.lbCategory = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.lbPrice = new System.Windows.Forms.Label();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.lbDescription = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lbName = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMenu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCategory)).BeginInit();
+            this.groupContent.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // dgvMenu
+            // 
+            this.dgvMenu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMenu.Location = new System.Drawing.Point(6, 19);
+            this.dgvMenu.Name = "dgvMenu";
+            this.dgvMenu.Size = new System.Drawing.Size(392, 462);
+            this.dgvMenu.TabIndex = 0;
+            // 
+            // dgvCategory
+            // 
+            this.dgvCategory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCategory.Location = new System.Drawing.Point(6, 19);
+            this.dgvCategory.Name = "dgvCategory";
+            this.dgvCategory.Size = new System.Drawing.Size(288, 162);
+            this.dgvCategory.TabIndex = 1;
+            // 
+            // groupContent
+            // 
+            this.groupContent.Controls.Add(this.btnUpdate);
+            this.groupContent.Controls.Add(this.btnDeactive);
+            this.groupContent.Controls.Add(this.btnAddNew);
+            this.groupContent.Controls.Add(this.lbModifiedTime);
+            this.groupContent.Controls.Add(this.lbCategory);
+            this.groupContent.Controls.Add(this.txtPrice);
+            this.groupContent.Controls.Add(this.lbPrice);
+            this.groupContent.Controls.Add(this.txtDescription);
+            this.groupContent.Controls.Add(this.lbDescription);
+            this.groupContent.Controls.Add(this.txtName);
+            this.groupContent.Controls.Add(this.lbName);
+            this.groupContent.Location = new System.Drawing.Point(422, 205);
+            this.groupContent.Name = "groupContent";
+            this.groupContent.Size = new System.Drawing.Size(300, 294);
+            this.groupContent.TabIndex = 2;
+            this.groupContent.TabStop = false;
+            this.groupContent.Text = "MealDetails";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(111, 253);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 10;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnDeactive
+            // 
+            this.btnDeactive.Location = new System.Drawing.Point(205, 253);
+            this.btnDeactive.Name = "btnDeactive";
+            this.btnDeactive.Size = new System.Drawing.Size(75, 23);
+            this.btnDeactive.TabIndex = 9;
+            this.btnDeactive.Text = "Deactive";
+            this.btnDeactive.UseVisualStyleBackColor = true;
+            // 
+            // btnAddNew
+            // 
+            this.btnAddNew.Location = new System.Drawing.Point(18, 253);
+            this.btnAddNew.Name = "btnAddNew";
+            this.btnAddNew.Size = new System.Drawing.Size(75, 23);
+            this.btnAddNew.TabIndex = 8;
+            this.btnAddNew.Text = "Add New";
+            this.btnAddNew.UseVisualStyleBackColor = true;
+            // 
+            // lbModifiedTime
+            // 
+            this.lbModifiedTime.AutoSize = true;
+            this.lbModifiedTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbModifiedTime.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lbModifiedTime.Location = new System.Drawing.Point(80, 220);
+            this.lbModifiedTime.Name = "lbModifiedTime";
+            this.lbModifiedTime.Size = new System.Drawing.Size(80, 13);
+            this.lbModifiedTime.TabIndex = 7;
+            this.lbModifiedTime.Text = "last modified on";
+            this.lbModifiedTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbCategory
+            // 
+            this.lbCategory.AutoSize = true;
+            this.lbCategory.Location = new System.Drawing.Point(15, 195);
+            this.lbCategory.Name = "lbCategory";
+            this.lbCategory.Size = new System.Drawing.Size(52, 13);
+            this.lbCategory.TabIndex = 6;
+            this.lbCategory.Text = "Category:";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(80, 157);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(200, 20);
+            this.txtPrice.TabIndex = 5;
+            // 
+            // lbPrice
+            // 
+            this.lbPrice.AutoSize = true;
+            this.lbPrice.Location = new System.Drawing.Point(15, 160);
+            this.lbPrice.Name = "lbPrice";
+            this.lbPrice.Size = new System.Drawing.Size(34, 13);
+            this.lbPrice.TabIndex = 4;
+            this.lbPrice.Text = "Price:";
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(80, 67);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(200, 70);
+            this.txtDescription.TabIndex = 3;
+            // 
+            // lbDescription
+            // 
+            this.lbDescription.AutoSize = true;
+            this.lbDescription.Location = new System.Drawing.Point(15, 70);
+            this.lbDescription.Name = "lbDescription";
+            this.lbDescription.Size = new System.Drawing.Size(63, 13);
+            this.lbDescription.TabIndex = 2;
+            this.lbDescription.Text = "Description:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(80, 27);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(200, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // lbName
+            // 
+            this.lbName.AutoSize = true;
+            this.lbName.Location = new System.Drawing.Point(15, 30);
+            this.lbName.Name = "lbName";
+            this.lbName.Size = new System.Drawing.Size(38, 13);
+            this.lbName.TabIndex = 0;
+            this.lbName.Text = "Name:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dgvMenu);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(404, 487);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Menu";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dgvCategory);
+            this.groupBox2.Location = new System.Drawing.Point(422, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(300, 187);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Category";
             // 
             // MenuForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(734, 461);
+            this.ClientSize = new System.Drawing.Size(734, 511);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupContent);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "MenuForm";
             this.Text = "MenuForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMenu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCategory)).EndInit();
+            this.groupContent.ResumeLayout(false);
+            this.groupContent.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvMenu;
+        private System.Windows.Forms.DataGridView dgvCategory;
+        private System.Windows.Forms.GroupBox groupContent;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lbName;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Label lbDescription;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Label lbPrice;
+        private System.Windows.Forms.Label lbCategory;
+        private System.Windows.Forms.Label lbModifiedTime;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDeactive;
+        private System.Windows.Forms.Button btnAddNew;
     }
 }
